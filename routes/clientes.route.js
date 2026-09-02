5367adef685d33b96fd526caac21f8e61b71aa92
facundo1000 <martinez.facundo85@gmail.com>
2026-09-02 19:38:57 -0300

    add: creacion de cada ruta de los controllers

---
+++ routes/clientes.route.js
@@ -0,0 +1,11 @@
+
+const router = require('express').Router();
+const clientesController = require('../controllers/clientes.controller');
+
+router.get('/', clientesController.getClientes);
+router.get('/:id', clientesController.getClienteById);
+router.post('/', clientesController.createCliente);
+router.put('/:id', clientesController.updateCliente);
+router.delete('/:id', clientesController.deleteCliente);
+
+module.exports = router;
